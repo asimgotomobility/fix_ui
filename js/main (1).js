c733cc38f82b2d4a74783576579e927ab50112e2
Asim Nasar Siddiqui <asim.siddiqui@gotomobility.in>
2021-05-01 16:49:35 +0530

    first commit

---
+++ js/main (1).js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay: true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false,
+            dots: false
+        },
+        600:{
+            items:1,
+            nav:false,
+            dots: false
+        },
+        1000:{
+            items:1,
+            nav:false,
+            dots: false
+        }
+    }
+})
